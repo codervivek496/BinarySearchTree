fd3dfb008ea718ec869c3aa5936773858bf6efca
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-30 11:00:08 +0530

    Added to main

---
+++ BST/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Binary Search Tree program");
+        }
+    }
+}
